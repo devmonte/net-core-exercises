c27254b39c86d99b48e32b10323c2a6d3c18f9d4
monte <greg.jonca@gmail.com>
2019-10-05 17:29:02 +0200

    Add 03 exercise

---
+++ src/03_Middleware/RequestQueryMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Middleware
+{
+    public class RequestQueryMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestQueryMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Query.Keys.Count > 0)
+            {
+                foreach(var key in context.Request.Query.Keys)
+                {
+                    var data = context.Request.Query[key];
+                    if (!string.IsNullOrWhiteSpace(data))
+                    {
+                        context.Request.Headers.Append(key, data);
+                    }
+                }
+            }
+            await _next(context);
+        }
+    }
+}
---
+++ src/03_Middleware/ReturnAllHeadersToResponseMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Middleware
+{
+    public class ReturnAllHeadersToResponseMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ReturnAllHeadersToResponseMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            
+            foreach(var header in context.Request.Headers)
+            {
+                await context.Response.WriteAsync($"\n {header.Key}: {header.Value}");
+            }
+
+            await _next(context);
+        }
+    }
+}
