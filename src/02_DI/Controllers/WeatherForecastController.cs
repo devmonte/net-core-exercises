788d165b170e2ec3dc4adaaade6d9fb60121024b
monte <greg.jonca@gmail.com>
2019-10-05 17:16:12 +0200

    Add 02_DI example

---
+++ src/02_DI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _02_DI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace _02_DI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IEnumerable<IWeatherService> _weatherServices;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IEnumerable<IWeatherService> weatherServices)
+        {
+            _logger = logger;
+            _weatherServices = weatherServices;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string weather = "";
+            foreach(var weatherService in _weatherServices)
+            {
+                weather += weatherService.GetWeather();
+            }
+
+            return Ok(weather);
+        }
+    }
+}
---
+++ src/02_DI/Services/IWeatherService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_DI.Services
+{
+    public interface IWeatherService
+    {
+        string GetConcreteWeather();
+        string GetWeather() => GetConcreteWeather();
+    }
+}
