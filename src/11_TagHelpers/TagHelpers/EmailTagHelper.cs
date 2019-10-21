07f6553b89cabbb1d0bf5f441870d1749791f4ec
devmonte <greg.jonca@gmail.com>
2019-10-21 09:54:35 +0200

    Add tag helper example

--- src/11_TagHelpers/TagHelpers/EmailTagHelper.cs
+++ src/11_TagHelpers/TagHelpers/EmailTagHelper.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _11_TagHelpers.TagHelpers
+namespace CustomTags
 {
     public class EmailTagHelper : TagHelper
     {
